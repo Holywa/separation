991d4b47ec4ec7d34d86c45bfc7890cbd66f6666
lilireli <orelie.suzanne@orange.fr>
2013-05-09 18:49:14 +0200

    library of gestures added

---
+++ Récits pour le web/lib/separation-gestures-v1.0.0.js
@@ -0,0 +1,36 @@
+/*
+ * Gestures for the Separation Project
+ */
+
+ var Gestures = {
+ 	cut: function(_x, _y, _width, _height, _layer, _stage){
+        var rect = new Kinetic.Rect({
+          x: _x,
+          y: _y,
+          width: _width,
+          height: _height,
+          fill: 'green',
+          stroke: 'black',
+          strokeWidth: 2
+        });
+
+        var x = 0;
+
+        this.on = function(handler) {
+          rect.on('touchmove', function(){
+            var touchPos = _stage.getTouchPosition();
+
+            if(touchPos.x >= (_width + _x - 20)){ x = _width + _x; }
+            else if((x != 0) && (touchPos.x <= (_x + 20))){
+              x = 0;
+              handler()
+            }  
+            else if((touchPos.x < x) && ((touchPos.y < (_height + _y)) && (touchPos.y > _y))){ x = touchPos.x; }
+            else{ x = 0; }
+          });
+        }
+
+        _layer.add(rect);
+      }
+ };
+ 
