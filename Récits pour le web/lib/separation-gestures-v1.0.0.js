da41c4d60cbc4c0866c2c3b30a7ee3f3b6056a5d
Olivia Reaney <olivia.reaney@live.fr>
2013-05-14 22:58:28 +0200

    ajout d'un index.html avec redirection vers main
    (passer à travers problème de timeOut pour appli mobile)

--- Récits pour le web/lib/separation-gestures-v1.0.0.js
+++ Récits pour le web/lib/separation-gestures-v1.0.0.js
@@ -3,6 +3,8 @@
  */
 
  var Gestures = {};
+ 
+ var mouseDown = false;
 
  Gestures.cut = function(parameters, layer, stage){
   var rect = new Kinetic.Rect({
@@ -16,17 +18,45 @@
   var x = 0;
 
   this.on = function(handler) {
-    rect.on('touchmove', function(){
-      var touchPos = stage.getTouchPosition();
+	rect.on('mousedown', function(evt) {
+		mouseDown = true;
+	});
+	
+	rect.on('mouseup', function(evt) {
+		mouseDown = false;
+	});
+  
+    rect.on('touchmove', function(evt){
+	  var touchPos = stage.getTouchPosition();
 
-      if(touchPos.x >= (parameters.width + parameters.x - 20)){ x = parameters.width + parameters.x; }
-      else if((x != 0) && (touchPos.x <= (parameters.x + 20))){
+      if(touchPos.x >= (parameters.width + parameters.x - 20)) {
+		x = parameters.width + parameters.x;
+		}
+      else if((x != 0) && (touchPos.x <= (parameters.x + 20))) {
         x = 0;
         handler()
       }  
       else if((touchPos.x < x) && ((touchPos.y < (parameters.height + parameters.y)) && (touchPos.y > parameters.y))){ x = touchPos.x; }
       else{ x = 0; }
     });
+	
+	rect.on('mousemove', function(evt){
+		if(mouseDown) {
+			var mousePos = stage.getMousePosition();
+			
+			if(mousePos.x >= (parameters.width + parameters.x - 20)){
+				x = parameters.width + parameters.x;
+				}
+			else if((x != 0) && (mousePos.x <= (parameters.x + 20))) {
+        x = 0;
+        handler()
+		}  
+		else if((mousePos.x < x) && ((mousePos.y < (parameters.height + parameters.y)) && (mousePos.y > parameters.y))) {
+			x = mousePos.x; }
+		else{ x = 0; }
+	  }
+    });
+	
   }
 
   layer.add(rect);
