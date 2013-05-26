3e5fe05d8afd0c093d6877b4ad623cb0a79b251c
mawie <colletmar1@gmail.com>
2013-05-26 23:03:47 +0200

    labo des mots, début

--- script/laboratory.js
+++ script/laboratory.js
@@ -3,4 +3,194 @@ function getLaboratoryMenu() {
 	setHomeBtn();
 	
 	alert("laboratory");
+}var mainLayer;
+var actionLayer;
+var titleSize;
+var textSize;
+var wordSize;
+var userWord;
+var wordsArray;
+var displayedWord;
+var wordChoosenH;
+var wordChoosenB;
+var wordsKineticGroup;
+
+
+
+
+function getLaboratoryMenu() {
+	clearStage(); 
+	
+	displayedWord = prompt("gimme your text");
+
+/*
+	stage.on('click', function(evt) {
+	   this.setText(prompt('New Text:'));
+	   layer.draw(); //redraw the layer containing the textfield
+	   
+	});
+*/
+	
+
+	
+	var xhr=getXMLHttpRequest();
+	xmlWordsFile=loadXMLDoc("words_requestResult.xml");
+	
+	wordsArray=new Array();
+	wordsNodes=xmlWordsFile.getElementsByTagName("word");
+	for(i=0;i<wordsNodes.length;i++){
+		wordsArray.push(wordsNodes[i]);
+	}
+	initLaboMenu();
+	//var wordsArraySorted=sort(wordsNodes);
+	//alert(wordsArray[9].getAttribute("name"));
+
+	setHomeBtn();
+	
+	stage.add(mainLayer);
+	stage.add(actionLayer);
+	mainLayer.draw();
+	
 }
+
+function displayWordArray(){ // les mettre sur la droite
+	//alert(wordsArray[1].getAttribute("name"));
+	var x = 0;
+	var y = 0;
+	var last;
+	wordsKineticGroup = new Kinetic.Group();
+	for(var index = 0; index < wordsArray.length; index++) {
+		var wordToChoose=new Array();
+		if(index == 0) {
+			wordToChoose[i] = new Kinetic.Text( {
+				fontFamily : 'DemiHaut',
+				fontSize : textSize,
+				fill : "#FFF",
+				text : wordsArray[index].getAttribute("name") + "\n",
+				align : "top",
+				x : stage.getWidth()*0.75,
+				y : stage.getHeight()*0.2
+			} );
+		var word_size = wordToChoose[i].getWidth();
+		wordToChoose[i].setOffset({ x: word_size });
+		}
+
+		else {
+			wordToChoose[i] = new Kinetic.Text( {
+				fontFamily : 'DemiHaut',
+				fontSize : textSize,
+				fill : "#FFF",
+				text : wordsArray[index].getAttribute("name") + "  ",
+				x : last.getX(),
+				y : last.getY() + last.getHeight()
+			} );
+		}
+		
+		
+
+		
+		
+		mainLayer.add(wordToChoose[i]);
+		last = wordToChoose[i];		
+		
+		wordToChoose[i].on("tap click", function(evt) {
+			//anim sur le mot wordChoosen *****************************************************************************************************
+			//mettre les animations pour trouver le bon mot
+			alert(wordToChoose[i].getText());
+			wordChoosenH.setText(wordToChoose[i].getText()); 
+			wordChoosenB.setText(wordToChoose[i].getText()); 
+			//displayWordArray();
+			//stage.add(mainLayer);
+			//stage.add(actionLayer);
+			mainLayer.draw();
+		} );
+		wordsKineticGroup.add(wordToChoose[i]);
+	}
+	
+	//mainLayer.draw();
+}
+
+
+//Creating the main menu
+function initLaboMenu() {
+	mainLayer.removeChildren();
+	actionLayer.removeChildren();
+
+	title = new Kinetic.Text( {
+		text : "Le laboratoire des mots",
+		fontFamily : "Century Gothic",
+		fontSize : titleSize,
+		fill : "#FFF",
+		align : "top",
+		x : stage.getWidth()/2,
+		y : stage.getHeight()*0.05
+	} );
+	title.setOffset( title.getWidth()/2, 0 );
+	
+	wordChoosenH = new Kinetic.Text( {
+		text : displayedWord,
+		fontFamily : "DemiHautH",
+		fontSize : textSize/2,
+		fill : "#FFF",
+		align : "left",
+		x : stage.getWidth()/2,
+		y : stage.getHeight()*0.275
+	} );
+	wordChoosenH.setOffset( wordChoosenH.getWidth()/2, wordChoosenH.getHeight()/2 );
+	
+	wordChoosenB = new Kinetic.Text( {
+		text : displayedWord,
+		fontFamily : "DemiHautB",
+		fontSize : textSize/2,
+		fill : "#FFF",
+		align : "left",
+		offset : { x :wordChoosenH.getWidth()/2,
+					y : wordChoosenH.getHeight()/2 },
+		x : stage.getWidth()/2,
+		y : wordChoosenH.getY()+stage.getHeight()*0.025
+	} );
+	
+	wordChoosen = new Kinetic.Rect( {
+		listening : true,
+		width : wordChoosenH.getWidth()*1.5,
+		height : wordChoosenH.getHeight() + wordChoosenB.getHeight(),
+		x : wordChoosenH.getX()-(wordChoosenH.getWidth()*0.5)+(wordChoosenH.getWidth()*0.25),
+		y : wordChoosenH.getY(),
+		offset : wordChoosenH.getOffset(),
+		opacity : 0.5,
+	} );
+
+	
+	
+	wordChoosen.on("tap click", function(evt) {
+		//anim sur le mot wordChoosen *****************************************************************************************************
+		//mettre les animations pour trouver le bon mot
+
+		displayWordArray();
+		stage.add(mainLayer);
+		stage.add(actionLayer);
+		mainLayer.draw();
+	} );
+	
+	getLaboMenu();
+}
+
+function getLaboMenu() {
+	mainLayer.add(title);
+	mainLayer.add(wordChoosenH);
+	mainLayer.add(wordChoosenB);
+
+	actionLayer.add(wordChoosen);
+	
+	mainLayer.draw();
+	actionLayer.draw();
+}
+
+
+/*
+TODO
+mettre groupe de mots dans un rectangle
+scroller ce rectangle
+modifier l'opacité de ce rectangle en haut, en bas...
+
+*/
--- script/libs/separation-toolkit-v1.0.js
+++ script/libs/separation-toolkit-v1.0.js
@@ -145,6 +145,147 @@ Separation.horizontal_move = function(params){
   }
 }
 
+
+/*																!----------------------------------------------------------------!
+ * détecter des mouvements verticaux
+ *
+ * @param {Object} paramètres pour dessiner le rectangle : taille du rectangle, placement du rectangle
+ * fonction utilisée pour coder cut et rub
+ */
+Separation.vertical_move = function(params){
+  var rTl = 0;
+  var lTr = 0;
+  var x = 0;
+  var oldx = 0;
+
+  var section = params.width / 4;
+
+  function inRectangle(touchPos){
+    if(
+      ((touchPos.x > params.x) && (touchPos.x < (params.x + params.width))) &&
+      ((touchPos.y > params.y) && (touchPos.y < (params.y + params.height))) 
+    ){
+      return true;
+    } else { return false; }
+  }
+
+  this.downToUp = function(handler){
+    switch(rTl){
+      case 0:
+        if(y > (params.y + section * 3)){ 
+          rTl = 1; 
+        }
+        break;
+
+      case 1:
+        if(y < oldy){
+          if((y > (params.y + section * 2)) && (y < (params.y + section * 3))){
+            rTl = 2;
+          }
+        } else { rTl = 0; }
+        break;
+
+      case 2:
+        if(y < oldy){
+          if((y > (params.y + section)) && (y < (params.y + section * 2))){
+            rTl = 3;
+          }
+        } else { rTl = 0; }
+        break;
+
+      case 3:
+        if(y < oldy){
+          if(y < (params.y + section)){
+            handler()
+            rTl = 0;
+          }
+        } else { rTl = 0; }
+        break;
+    };
+  }
+
+  this.upToDown = function(handler){
+    switch(lTr){
+      case 0:
+        if(y < params.y + section){ 
+          lTr = 1; 
+        }
+        break;
+
+      case 1:
+        if(y > oldx){
+          if((y > (params.y + section)) && (y < (params.y + section * 2))){
+            lTr = 2;
+          }
+        } else { lTr = 0; }
+        break;
+
+      case 2:
+        if(y > oldx){
+          if((y > (params.y + section * 2)) && (y < (params.y + section * 3))){
+            lTr = 3;
+          }
+        } else { lTr = 0; }
+        break;
+
+      case 3:
+        if(y > oldy){
+          if(y > params.y + section * 3){
+            handler()
+            lTr = 0;
+          }
+        } else { lTr = 0; }
+        break;
+    };
+  }  
+
+  this.on = function(handler){
+    function corpus(touchPos){
+      y = touchPos.y;
+
+      if(inRectangle(touchPos) == true){
+        handler()
+      }
+      else {
+        rTl = 0;
+        lTr = 0;
+      }
+
+      oldy = y;
+    };
+
+    // touch event
+    function detect_touch(event){
+      event.preventDefault;
+
+      var touchPos = {
+        x: event.touches[0].pageX,
+        y: event.touches[0].pageY
+      }
+      corpus(touchPos);
+    };
+
+    window.addEventListener("touchmove", detect_touch, false);
+
+    // mouse event
+    function detect_mouse(event){
+      if(event.which == 1){ // la souris est clickée
+        var touchPos = {
+          x: event.clientX,
+          y: event.clientY
+        }
+        corpus(touchPos);
+      }
+    };
+
+    window.addEventListener("mousemove", detect_mouse, false);
+  }
+}
+
+
+
+
+
 /*
  * Geste qui détecte la séparation d'un mot
  *
@@ -389,5 +530,30 @@ Separation.onZone = function(params){
 
     window.addEventListener('touchmove', detect, false);
 
+  }
+};
+
+
+
+
+
+
+
+/*
+ * Geste qui détecte le scroll sur un mot  !----------------------------------------------------------------!
+ *
+ * @param {Object} paramètres pour dessiner le rectangle : taille du rectangle, placement du rectangle
+ * @param {Text} type de coupure : "u_d" pour up to down, "d_u" pour le contraire, toutes les autres valeurs pour les deux sens
+ * déclenche la fonction handler dès que la fonction repère un mouvement de coupure
+ * faire attention à définir la fonction après les autres variables pour que le rectangle soit au premier plan
+ */
+Separation.scroll = function(params, type){
+  var detect = new Separation.horizontal_move(params);
+
+  this.on = function(handler) {
+    detect.on(function(){
+      if (type != "u_d") {detect.upToDown(handler);}
+      if (type != "d_u") {detect.downToUp(handler);} 
+    });
   }
 };
