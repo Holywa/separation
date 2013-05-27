12a53f7887348201def502ba79f38976be5e627f
lilireli <orelie.suzanne@orange.fr>
2013-05-27 14:08:46 +0200

    added groups for animations

--- script/tutoriel.js
+++ script/tutoriel.js
@@ -5,13 +5,17 @@ function getTutorielMenu() {
   var lines = stage.getHeight() / 6;
   var col = stage.getWidth() / 2;
 
-  var size_font = 4;
+  var size_font = 5;
   var demihaut_font = 16 * size_font;
   var demihaut_part_font = 5 * size_font;
-  var decal_h = 1.5 * size_font;
+  var centrale_font = 4.2 * size_font;
+  var decal_h = 1.2 * size_font;
   var decal_b = 2 * size_font;
   var image_y = 1.25 * size_font;
   var image_dimension = 0.05 * size_font; 
+  var decal_c_h = 0.4 * size_font;
+  var decal_c_c = 2.25 * size_font;
+  var decal_c_b = 4.75 * size_font;
 
   /* 
    * ligne 1
@@ -68,10 +72,13 @@ function getTutorielMenu() {
     y: - p1_2_c.getHeight() * 2 + decal_b
   });
 
+  var cut_group = new Kinetic.Group();
+  cut_group.add(p1_2_a);
+  cut_group.add(p1_2_b);
+  cut_group.add(p1_2_c);
+
+  actionLayer.add(cut_group);
   actionLayer.add(p1_1);
-  actionLayer.add(p1_2_a);
-  actionLayer.add(p1_2_b);
-  actionLayer.add(p1_2_c);
 
   /*
    * ligne 2
@@ -140,9 +147,12 @@ function getTutorielMenu() {
   p3_2_b.setOffset({ x: word_size_3 - p3_1.getWidth() });
   p3_3.setOffset({ x: word_size_3 - p3_1.getWidth() - p3_2_a.getWidth() });
 
+  var rub_group = new Kinetic.Group();
+  rub_group.add(p3_2_a);
+  rub_group.add(p3_2_b);
+
+  actionLayer.add(rub_group);
   actionLayer.add(p3_1);
-  actionLayer.add(p3_2_b);
-  actionLayer.add(p3_2_a);
   actionLayer.add(p3_3);
 
   /*
@@ -151,43 +161,44 @@ function getTutorielMenu() {
   var p4_1_a = new Kinetic.Text({
     x: col,
     y: 4*lines,
-    text: 'shreds ',
-    fontSize: demihaut_part_font,
-    fontFamily: 'CentraleMinH',
+    text: 'SHREDS',
+    fontSize: centrale_font,
+    fontFamily: 'CentraleH',
     fill: '#FFF'
   });
 
   var p4_1_b = new Kinetic.Text({
     x: col,
     y: 4*lines,
-    text: 'shreds ',
-    fontSize: demihaut_part_font,
-    fontFamily: 'CentraleMinC',
+    text: 'SHREDS',
+    fontSize: centrale_font,
+    fontFamily: 'CentraleC',
     fill: '#FFF'
   });
 
   var p4_1_c = new Kinetic.Text({
     x: col,
     y: 4*lines,
-    text: 'chases ',
-    fontSize: demihaut_part_font,
-    fontFamily: 'CentraleMinC',
-    fill: '#FFF'
+    text: 'SHREDS',
+    fontSize: centrale_font,
+    fontFamily: 'CentraleC',
+    fill: '#FFF',
+    opacity: 0
   });
 
   var p4_1_d = new Kinetic.Text({
     x: col,
     y: 4*lines,
-    text: 'shreds ',
-    fontSize: demihaut_part_font,
-    fontFamily: 'CentraleMinC',
+    text: 'SHREDS',
+    fontSize: centrale_font,
+    fontFamily: 'CentraleB',
     fill: '#FFF'
   });
 
   var p4_2 = new Kinetic.Text({
     x: col,
     y: 4*lines,
-    text: 'me',
+    text: ' ME',
     fontSize: demihaut_font,
     fontFamily: 'DemiHaut',
     fill: '#FFF' 
@@ -195,16 +206,31 @@ function getTutorielMenu() {
 
   var word_size_4 = (p4_1_a.getWidth() + p4_2.getWidth()) / 2;
 
-  p4_1_a.setOffset({ x: word_size_4 });
-  p4_1_b.setOffset({ x: word_size_4 });
-  p4_1_c.setOffset({ x: word_size_4 });
-  p4_1_d.setOffset({ x: word_size_4 });
+  p4_1_a.setOffset({ 
+    x: word_size_4,
+    y: - p4_1_a.getHeight() + decal_c_h
+  });
+  p4_1_b.setOffset({ 
+    x: word_size_4,
+    y: - 2 * p4_1_b.getHeight() + decal_c_c
+  });
+  p4_1_c.setOffset({ 
+    x: word_size_4,
+    y: - 2 * p4_1_c.getHeight() + decal_c_c
+  });
+  p4_1_d.setOffset({ 
+    x: word_size_4,
+    y: - 3 * p4_1_d.getHeight() + decal_c_b
+  });
   p4_2.setOffset({ x: word_size_4 - p4_1_a.getWidth() });
 
-  actionLayer.add(p4_1_a);
-  actionLayer.add(p4_1_b);
-  actionLayer.add(p4_1_c);
-  actionLayer.add(p4_1_d);
+  var tear_group = new Kinetic.Group();
+  tear_group.add(p4_1_a);
+  tear_group.add(p4_1_b);
+  tear_group.add(p4_1_c);
+  tear_group.add(p4_1_d);
+
+  actionLayer.add(tear_group);
   actionLayer.add(p4_2);
 
   /*
@@ -229,25 +255,25 @@ function getTutorielMenu() {
     tween.play();
   }
 
-  function node_zoom(node, x){
+  function node_zoom(node, x, y){
     var tween = new Kinetic.Tween({
       node: node,
       duration: 1,
       scaleX: 2,
       scaleY: 2,
       x: x,
-      y: 2*lines,
+      y: y
     });
     tween.play();
   }
 
-  function node_unzoom(node, y){
+  function node_unzoom(node, x, y){
     var tween = new Kinetic.Tween({
       node: node,
       duration: 1,
       scaleX: 1,
       scaleY: 1,
-      x: col,
+      x: x,
       y: y
     });
     tween.play();
@@ -275,32 +301,16 @@ function getTutorielMenu() {
   if(lock == 0 || lock == 1){
     var cut_activation = function(){
       darkenize();
-
-      node_dark(p3_2_a);
-      node_dark(p3_2_b);
-      /*node_dark(p4_1_a);
-      node_dark(p4_1_b);
-      node_dark(p4_1_c);
-      node_dark(p4_1_d);*/
-
-      node_zoom(p1_2_a, col - p1_2_a.getWidth() / 2);
-      node_zoom(p1_2_b, col - p1_2_a.getWidth() / 2);
-      node_zoom(p1_2_c, col - p1_2_a.getWidth() / 2);
+      node_zoom(cut_group, - stage.getWidth() / 1.65, - stage.getHeight() / 100);
+      node_dark(rub_group);
+      node_dark(tear_group); 
     }
 
     var cut_desactivation = function(){
       illuminate();
-
-      node_light(p3_2_a);
-      node_light(p3_2_b);
-      /*node_light(p4_1_a);
-      node_light(p4_1_b);
-      node_light(p4_1_c);
-      node_light(p4_1_d);*/
-
-      node_unzoom(p1_2_a, lines);
-      node_unzoom(p1_2_b, lines);
-      node_unzoom(p1_2_c, lines);
+      node_unzoom(cut_group, 0, 0);
+      node_light(rub_group);
+      node_light(tear_group);      
     }
 
     function cutting(){
@@ -362,17 +372,7 @@ function getTutorielMenu() {
       }
     }
 
-    p1_2_a.on('tap click', function(){
-      var test = new Date();
-      cutting();
-    });
-
-    p1_2_b.on('tap click', function(){
-      var test = new Date();
-      cutting();
-    });
-
-    p1_2_c.on('tap click', function(){
+    cut_group.on('tap click', function(){
       var test = new Date();
       cutting();
     });
@@ -396,35 +396,19 @@ function getTutorielMenu() {
   if(lock == 0 || lock == 2){
     var rub_activation = function(){
       darkenize();
-
-      node_dark(p1_2_a);
-      node_dark(p1_2_b);
-      node_dark(p1_2_c);
-      /*node_dark(p4_1_a);
-      node_dark(p4_1_b);
-      node_dark(p4_1_c);
-      node_dark(p4_1_d);*/
-
-      node_zoom(p3_2_a, col + p3_2_a.getWidth() / 2);
-      node_zoom(p3_2_b, col + p3_2_b.getWidth() / 2);
+      node_dark(cut_group);
+      node_zoom(rub_group, - stage.getWidth() / 2.40, - stage.getHeight() / 1.5);
+      node_dark(tear_group);
     }
 
     var rub_desactivation = function(){
       illuminate();
-
-      node_light(p1_2_a);
-      node_light(p1_2_b);
-      node_light(p1_2_c);
-      /*node_light(p4_1_a);
-      node_light(p4_1_b);
-      node_light(p4_1_c);
-      node_light(p4_1_d);*/
-
-      node_unzoom(p3_2_a, 3*lines + image_y);
-      node_unzoom(p3_2_b, 3*lines + image_y);
+      node_light(cut_group);
+      node_unzoom(rub_group, 0, 0);
+      node_light(tear_group);
     }
 
-    p3_2_a.on('tap click', function(){
+    rub_group.on('tap click', function(){
       if(lock == 0){
         lock = 2;
         rub_activation();
@@ -509,19 +493,95 @@ function getTutorielMenu() {
 
   // tear
   if(lock == 0 || lock == 3){
-    p4_1_a.on('tap', function(){
+    var tear_activation = function(){
+      darkenize();
+      node_dark(cut_group);
+      node_dark(rub_group);
+      node_zoom(tear_group, - stage.getWidth() / 2.45, - stage.getHeight());
+    }
+
+    var tear_desactivation = function(){
+      illuminate();
+      node_light(cut_group);
+      node_light(rub_group);
+      node_unzoom(tear_group, 0, 0);
+    }
+
+    function tearing(){
+      if(lock == 0){
+        lock = 3;
+        tear_activation();
+
+        var dechirer = new Separation.tear({
+          x: col - p4_1_a.getWidth(),
+          y: 2*lines + p4_1_a.getHeight(),
+          width: p4_1_a.getWidth() * 2,
+          height: p4_1_a.getHeight() * 6
+        });
+
+        var sens  = true;
+
+        function animation_tear(node1, node2){
+          var tween1 = new Kinetic.Tween({
+            node: node1,
+            duration: 2,
+            easing: Kinetic.Easings.StrongEaseInOut,
+            x: 0
+          })
+          tween1.play();
+
+          var tween2 = new Kinetic.Tween({
+            node: node2,
+            duration: 2,
+            easing: Kinetic.Easings.StrongEaseInOut,
+            x: 2
+          })
+          setTimeout(function(){
+            tween2.play();
+          }, 400)
+
+          setTimeout(function(){
+            tween1.finish();
+            tween2.finish(); 
+
+            node1.setAttrs({
+              x: col - p1_2_a.getWidth() / 2,
+              y: 2 * lines,
+              offsetX: - col - p1_2_c.getWidth() / 2
+            });
+          }, 2000);
+        }
+
+        dechirer.on(function(){
+          if(lock == 3){
+            if(sens == true){
+              animation_tear(p4_1_b, p4_1_c);
+              sens = false;
+            } else {
+              animation_tear(p4_1_c, p4_1_b);
+              sens = true;
+            }
+          }
+        });
+      }
+    }
+
+    tear_group.on('tap click', function(){
+      tearing();
     });
 
-    p4_1_b.on('tap', function(){
-
+    var tear_unZoom = new Separation.onZone({
+      x: col - p4_1_a.getWidth(),
+      y: 2*lines + p4_1_a.getHeight(),
+      width: p4_1_a.getWidth() * 2,
+      height: p4_1_a.getHeight() * 6
     });
 
-    p4_1_c.on('tap', function(){
-
-    });
-
-    p4_1_d.on('tap', function(){
-
+    tear_unZoom.on(function(){
+      if(lock == 3){
+        tear_desactivation();
+        lock = 0;
+      }
     });
   }
   
